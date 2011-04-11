7077c220f9d609fe924864c7ac848a8b1f4a0510
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2011-04-11 18:04:05 +0200

    * Dag Olav's bugfix pr say

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/utils/OnlineGuruDependencyModule.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/utils/OnlineGuruDependencyModule.java
@@ -4,10 +4,18 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import no.ntnu.online.onlineguru.utils.xmlrpcserver.StatefulXmlRpcServer;
 import no.ntnu.online.onlineguru.utils.xmlrpcserver.XmlRpcServer;
+import org.apache.log4j.Logger;
 
 public class OnlineGuruDependencyModule extends AbstractModule implements Module {
+    static Logger logger = Logger.getLogger(OnlineGuruDependencyModule.class);
+
     @Override
     protected void configure() {
         bind(XmlRpcServer.class).to(StatefulXmlRpcServer.class);
+        try {
+            bind(XmlRpcServer.class).toInstance(new StatefulXmlRpcServer());
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
     }
 }
