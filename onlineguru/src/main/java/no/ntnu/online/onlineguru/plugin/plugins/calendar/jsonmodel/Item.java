fd42e403e8fae0114622d6b14e90c3099f03e112
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2011-10-18 15:44:49 +0200

    a check to make sure the endDate is after or equal to startDate, logs an error if it isn't and logs the event which is being screwed/not working for debugging.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/calendar/jsonmodel/Item.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/calendar/jsonmodel/Item.java
@@ -1,6 +1,7 @@
 package no.ntnu.online.onlineguru.plugin.plugins.calendar.jsonmodel;
 
 import no.ntnu.online.onlineguru.plugin.plugins.calendar.Event;
+import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
@@ -18,6 +19,8 @@ public class Item {
     private static final Pattern EVENT_START_PATTERN = Pattern.compile(".*Første start.*(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2}).*");
     private static final Pattern EVENT_START_SPECIAL = Pattern.compile(".*Når: \\w+\\. (\\d{1,2})\\. (\\w+)\\. (\\d{4}) (\\d{2}):(\\d{2}) til (\\d{2}):(\\d{2}).*");
     private static final Pattern EVENT_LENGTH = Pattern.compile(".*Varighet: (\\d+).*");
+
+    static Logger logger = Logger.getLogger(Item.class);
 
     @Override
     public String toString() {
@@ -84,12 +87,18 @@ public class Item {
                         Integer.parseInt(matcher.group(6)),
                         Integer.parseInt(matcher.group(7)),
                         0, 0);
-                Interval intervalBetweenEvents = new Interval(eventStart, eventStop);
 
-                return ((int) (intervalBetweenEvents.toDurationMillis() / 1000));
+                if (eventStop.isEqual(eventStart) || eventStop.isAfter(eventStart)) {
+                    Interval intervalBetweenEvents = new Interval(eventStart, eventStop);
+
+                    return ((int) (intervalBetweenEvents.toDurationMillis() / 1000));
+                } else {
+                    logger.error(String.format("Error with Item : eventStop is before eventStart!\nstart: %s\nend: %s", eventStart.toString(), eventStop.toString()));
+                }
             }
 
         }
+        logger.error(String.format("Error with Item : %s\n details: %s", this, getDetails()));
         return -1;
     }
 
