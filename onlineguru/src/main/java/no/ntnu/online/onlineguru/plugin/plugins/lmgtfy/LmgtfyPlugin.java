5a7a0c3b44cfd6e8518b144f095b90246ca43ac9
Nina Margrethe Smørsgård <nina.m.smosgard@c2i.net>
2011-10-21 16:42:45 +0200

    Updated gitignore to ignore settings/ Added bitly dependency Updatet LmgtfyPlugin to use bitly to shorten long links.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/lmgtfy/LmgtfyPlugin.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/lmgtfy/LmgtfyPlugin.java
@@ -6,11 +6,18 @@ import no.fictive.irclib.event.model.EventType;
 import no.fictive.irclib.model.network.Network;
 import no.ntnu.online.onlineguru.plugin.control.EventDistributor;
 import no.ntnu.online.onlineguru.plugin.model.Plugin;
+import no.ntnu.online.onlineguru.utils.SimpleIO;
 import no.ntnu.online.onlineguru.utils.Wand;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import com.rosaloves.bitlyj.Url;
+import org.apache.log4j.Logger;
+
+import static com.rosaloves.bitlyj.Bitly.*;
 
 
 /**
@@ -23,16 +30,26 @@ public class LmgtfyPlugin implements Plugin {
 
     private final String DESCRIPTION = "Lmgtfy plugin";
     private final String LMGTFYTRIGGER = "!lmgtfy";
+    private final String settings_folder = "settings/";
+	private final String settings_file = settings_folder + "lmgtfy.conf";
 
     private Wand wand;
-    private String sourceUrl;
+    private String bitlyUsername;
+    private String bitlyApiKey;
+    static Logger logger = Logger.getLogger(LmgtfyPlugin.class);
 
-    private void Lmgtfy() {
+
+    public LmgtfyPlugin() {
         initiate();
     }
 
     private void initiate() {
-        sourceUrl = "";
+        try {
+            bitlyUsername = SimpleIO.loadConfig(settings_file).get("username");
+            bitlyApiKey = SimpleIO.loadConfig(settings_file).get("apikey");
+        } catch (IOException e) {
+            logger.error(e);
+        }
     }
 
     public String getDescription() {
@@ -62,13 +79,18 @@ public class LmgtfyPlugin implements Plugin {
 
     public String generateLmgtfyLink(List<String> message) {
         String link = "http://lmgtfy.com/?q=";
+        String sublink = "";
         for (String term : message) {
             term = term.trim();
             if (term.length() > 1)
-                link += term + "+";
+                sublink += term + "+";
         }
+        link += sublink;
         link = link.substring(0, link.length() - 1);
-
+        if (sublink.length() >= 42) {
+            Url url = as(bitlyUsername, bitlyApiKey).call(shorten(link));
+            link = url.getShortUrl();
+        }
         return link;
     }
 
