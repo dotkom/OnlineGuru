24276e900185eb651551e75ff869d699ef93230a
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-04 00:51:42 +0100

    Extracted bit.ly functionality.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/lmgtfy/LmgtfyPlugin.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/lmgtfy/LmgtfyPlugin.java
@@ -6,19 +6,12 @@ import no.fictive.irclib.event.model.EventType;
 import no.fictive.irclib.model.network.Network;
 import no.ntnu.online.onlineguru.plugin.control.EventDistributor;
 import no.ntnu.online.onlineguru.plugin.model.Plugin;
-import no.ntnu.online.onlineguru.utils.SimpleIO;
+import no.ntnu.online.onlineguru.utils.URLShortener;
 import no.ntnu.online.onlineguru.utils.Wand;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import com.rosaloves.bitlyj.Url;
-import org.apache.log4j.Logger;
-
-import static com.rosaloves.bitlyj.Bitly.*;
-
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,26 +24,11 @@ public class LmgtfyPlugin implements Plugin {
     public static final String LMGTFY_BASE = "http://lmgtfy.com/?q=";
     private final String DESCRIPTION = "Lmgtfy plugin";
     private final String LMGTFYTRIGGER = "!lmgtfy";
-    private final String settings_folder = "settings/";
-    private final String settings_file = settings_folder + "lmgtfy.conf";
 
     private Wand wand;
-    private String bitlyUsername;
-    private String bitlyApiKey;
-    static Logger logger = Logger.getLogger(LmgtfyPlugin.class);
-
 
     public LmgtfyPlugin() {
-        initiate();
-    }
 
-    private void initiate() {
-        try {
-            bitlyUsername = SimpleIO.loadConfig(settings_file).get("username");
-            bitlyApiKey = SimpleIO.loadConfig(settings_file).get("apikey");
-        } catch (IOException e) {
-            logger.error(e);
-        }
     }
 
     public String getDescription() {
@@ -81,7 +59,7 @@ public class LmgtfyPlugin implements Plugin {
 
                 String sublink = link.substring(LMGTFY_BASE.length(), link.length());
                 if (sublink.length() >= 42)
-                    link = bitlyfyLink(link);
+                    link = URLShortener.bitlyfyLink(link);
 
                 wand.sendMessageToTarget(network, target, link);
             }
@@ -99,11 +77,6 @@ public class LmgtfyPlugin implements Plugin {
 
         link = link.substring(0, link.length() - 1);
         return link;
-    }
-
-    public String bitlyfyLink(String link) {
-        Url url = as(bitlyUsername, bitlyApiKey).call(shorten(link));
-        return url.getShortUrl();
     }
 
     public void addEventDistributor(EventDistributor eventDistributor) {
