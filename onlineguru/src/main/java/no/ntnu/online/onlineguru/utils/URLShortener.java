8ae9f25f577f7d7e3e571c839268fcb15af2b96a
Håvard Slettvold <havardsv@stud.ntnu.no>
2012-03-04 00:50:54 +0100

    Extracted bit.ly shortener to a util.

---
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/utils/URLShortener.java
@@ -0,0 +1,41 @@
+package no.ntnu.online.onlineguru.utils;
+
+import com.rosaloves.bitlyj.Url;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.rosaloves.bitlyj.Bitly.as;
+import static com.rosaloves.bitlyj.Bitly.shorten;
+
+/**
+ * @author Håvard Slettvold
+ */
+public class URLShortener {
+
+    private static final String settings_folder = "settings/";
+    private static final String settings_file = settings_folder + "bitly.conf";
+
+    private static String bitlyUsername;
+    private static String bitlyApiKey;
+    static Logger logger = Logger.getLogger(URLShortener.class);
+
+    public static String bitlyfyLink(String link) {
+        initiate();
+
+        Url url = as(bitlyUsername, bitlyApiKey).call(shorten(link));
+        return url.getShortUrl();
+    }
+
+    private static void initiate() {
+        try {
+            Map<String, String> settings = SimpleIO.loadConfig(settings_file);
+            bitlyUsername = settings.get("username");
+            bitlyApiKey = settings.get("apikey");
+        } catch (IOException e) {
+            logger.error("Config load failed.", e.getCause());
+        }
+    }
+
+}
