0d791f5c14e92fbec478cd94933bbec133de17bc
melwil <slettvold@gmail.com>
2013-04-04 19:17:54 +0200

    Fixed tab spacing.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/VerifySettings.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/VerifySettings.java
@@ -18,12 +18,12 @@ import java.util.ArrayList;
 public class VerifySettings {
     static Logger logger = Logger.getLogger(VerifySettings.class);
     private static final String settings_folder = "settings/";
-	private static final String settings_file = settings_folder + "settings.conf";
+    private static final String settings_file = settings_folder + "settings.conf";
 
     private static ArrayList<ConnectionInformation> conInfoList;
 
-	protected static ArrayList<ConnectionInformation> readSettings() throws MissingSettingsException {
-		conInfoList = new ArrayList<ConnectionInformation>();
+    protected static ArrayList<ConnectionInformation> readSettings() throws MissingSettingsException {
+        conInfoList = new ArrayList<ConnectionInformation>();
 
         try {
             ArrayList<Settings> settingsList = SettingsReader.readSettings(settings_file);
@@ -45,14 +45,14 @@ public class VerifySettings {
             logger.error(mse.getError(), mse.getCause());
             System.exit(1);
         }
-		return conInfoList;
-	}
+        return conInfoList;
+    }
 
     private static void makeConnectionInformation(ArrayList<Settings> settingsList) {
         ConnectionInformation connectionInformation;
         Profile profile;
 
-        for(Settings settings : settingsList) {
+        for (Settings settings : settingsList) {
             try {
                 connectionInformation = new ConnectionInformation();
                 connectionInformation.setServeralias(settings.getSetting("server_alias"));
@@ -67,8 +67,8 @@ public class VerifySettings {
 
 
                 String channels = settings.getSetting("channels");
-                if(channels != null) {
-                    for(String channel : channels.split(",")) {
+                if (channels != null) {
+                    for (String channel : channels.split(",")) {
                         connectionInformation.addChannel(channel);
                     }
                 }
@@ -76,26 +76,27 @@ public class VerifySettings {
                 profile = makeProfile(settings);
                 connectionInformation.setProfile(profile);
 
-                if(connectionInformation.isValid() && profile.isValid()) {
+                if (connectionInformation.isValid() && profile.isValid()) {
                     conInfoList.add(connectionInformation);
                     logger.info("Settings are in order, connecting..");
-                } else {
+                }
+                else {
                     logger.error("Settings need to be populated properly.");
                 }
-            } catch(UnknownHostException unknownHostException) {
+            } catch (UnknownHostException unknownHostException) {
                 logger.error(String.format("Can't bind to selected hostname, skipping this connection entry. exception '%s'", unknownHostException.getMessage()));
             }
         }
     }
 
     private static Profile makeProfile(Settings settings) {
-        Profile profile = new Profile	(
-                            settings.getSetting("nickname"),
-                            settings.getSetting("alt_nickname"),
-                            settings.getSetting("realname"),
-                            settings.getSetting("ident"),
-                            settings.getSetting("email")
-                         );
+        Profile profile = new Profile(
+                settings.getSetting("nickname"),
+                settings.getSetting("alt_nickname"),
+                settings.getSetting("realname"),
+                settings.getSetting("ident"),
+                settings.getSetting("email")
+        );
 
         profile.setQuitMessage(settings.getSetting("quitmsg"));
         return profile;
@@ -105,25 +106,25 @@ public class VerifySettings {
         SimpleIO.createFolder(settings_folder);
         SimpleIO.createFile(settings_file);
 
-		File file = new File(settings_file);
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-		
-		writer.write("[network]\n");
-		writer.write("server_alias=\n");
-		writer.write("hostname=\n");
-		writer.write("port=\n");
+        File file = new File(settings_file);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+        writer.write("[network]\n");
+        writer.write("server_alias=\n");
+        writer.write("hostname=\n");
+        writer.write("port=\n");
         writer.write("ipv6=false\n");
         writer.write("#bind_address=\n");
-		writer.write("nickname=\n");
-		writer.write("alt_nickname=\n");
-		writer.write("ident=\n");
-		writer.write("realname=\n");
-		writer.write("email=\n");
-		writer.write("quitmsg=\n");
-		writer.write("channels=\n");
-		writer.write("# Example:\n");
-		writer.write("#channels=#online, #channelwithkey keyhere\n");
-		writer.close();
-	}
-	
+        writer.write("nickname=\n");
+        writer.write("alt_nickname=\n");
+        writer.write("ident=\n");
+        writer.write("realname=\n");
+        writer.write("email=\n");
+        writer.write("quitmsg=\n");
+        writer.write("channels=\n");
+        writer.write("# Example:\n");
+        writer.write("#channels=#online, #channelwithkey keyhere\n");
+        writer.close();
+    }
+
 }
