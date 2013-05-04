03e5dce9094a1b0048e01cedddd4e7100f2952bf
melwil <slettvold@gmail.com>
2013-05-05 01:13:38 +0200

    Updated model for Flag to accomodate more advanced operations. Added some more flags.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/flags/model/Flag.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/flags/model/Flag.java
@@ -5,6 +5,28 @@ package no.ntnu.online.onlineguru.plugin.plugins.flags.model;
  */
 public enum Flag {
 
-    ANYONE, o, O, v, V, B, b, K, L, m, t;
+    ANYONE (""),  // This flag is never given, only passed to allow anyone
+    B ("B"),  // can unban
+    b ("b"),  // can ban
+    f ("f"),  // can change channel flags
+    i ("i"),  // allows invites from the bot
+    K ("K"),  // can kick
+    L ("L"),  // can set channel limit
+    m ("m"),  // can moderate
+    o ("o"),  // can op
+    O ("O"),  // can deop
+    t ("t"),  // can use topic
+    v ("v"),  // can voice
+    V ("V");  // can unvoice
+
+    private final String value;
+
+    Flag(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return this.value;
+    }
 
 }
