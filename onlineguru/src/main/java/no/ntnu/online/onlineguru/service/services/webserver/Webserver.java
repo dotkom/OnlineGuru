06b438995b2757bb8a380c5a05188ce8c37c6908
melwil <slettvold@gmail.com>
2013-05-25 18:31:15 +0200

    Moved services from utils.

---
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/service/services/webserver/Webserver.java
@@ -0,0 +1,41 @@
+package no.ntnu.online.onlineguru.service.services.webserver;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author Roy Sindre Norangshol
+ */
+public class Webserver extends NanoHTTPD {
+    Map<String, WebserverCallback> services;
+    static Logger logger = Logger.getLogger(Webserver.class);
+
+
+    public Webserver() throws IOException {
+        super(9875);
+        services = new HashMap<String, WebserverCallback>();
+    }
+
+    public Response serve(String uri, String method, Properties header, Properties parms, Properties files) {
+        logger.info(String.format("uri: %s, method: %s, header: %s, parms: %s, files: %s", uri, method, header, parms, files));
+        if (services.containsKey(uri)) {
+            return services.get(uri).serve(uri, method, header, parms, files);
+        }
+        return new Response(HTTP_FORBIDDEN, MIME_PLAINTEXT, "No access");
+
+     }
+
+    public boolean registerWebserverCallback(String uri, WebserverCallback callback) {
+        if (!services.containsKey(uri)) {
+            services.put(uri, callback);
+            return true;
+        } else
+            return false;
+    }
+
+}
+
---
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/service/services/webserver/WebserverCallback.java
@@ -0,0 +1,11 @@
+package no.ntnu.online.onlineguru.service.services.webserver;
+
+import java.util.Properties;
+
+/**
+ * @author Roy Sindre Norangshol
+ */
+public interface WebserverCallback {
+    public Response serve(String uri, String method, Properties header, Properties parms, Properties files);
+    public void httpdServerShutdown(String message);
+}
