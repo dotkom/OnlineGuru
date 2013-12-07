d73c74b1555ade5b0c3d633ea67048b0bd3834f0
melwil <slettvold@gmail.com>
2013-12-07 02:29:25 +0100

    Bus info is now sent in private messages instead of channel.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/busbuddy/BusBuddyPlugin.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/busbuddy/BusBuddyPlugin.java
@@ -176,7 +176,7 @@ public class BusBuddyPlugin implements Plugin {
                 try {
                     logger.debug(String.format("BusBuddy asking '%s'", privMsgEvent.getMessage().toLowerCase().substring(triggerLength + 1).trim()));
                     answer = handleBusBuddyOracleRequest(privMsgEvent.getMessage().toLowerCase().substring(triggerLength + 1).trim());
-                    wand.sendMessageToTarget(privMsgEvent.getNetwork(), privMsgEvent.getTarget(), String.format("[BusBuddy] %s", answer.getAnswer().trim().replaceAll("\\r\\n|\\r|\\n", " ")));
+                    wand.sendMessageToTarget(privMsgEvent.getNetwork(), privMsgEvent.getSender(), String.format("[BusBuddy] %s", answer.getAnswer().trim().replaceAll("\\r\\n|\\r|\\n", " ")));
                     System.out.println(answer);
                     System.out.println(answer.getDestinationFrom());
                     if (answer.getDestinationFrom() != null) {
@@ -184,7 +184,7 @@ public class BusBuddyPlugin implements Plugin {
                     }
                 } catch (IOException e1) {
                     logger.error(String.format("Error while asking busbuddy oracle: %s", e1));
-                    wand.sendMessageToTarget(privMsgEvent.getNetwork(), privMsgEvent.getTarget(), String.format("[BusBuddy-ERR] IOException: %s", e1.getMessage()));
+                    wand.sendMessageToTarget(privMsgEvent.getNetwork(), privMsgEvent.getSender(), String.format("[BusBuddy-ERR] IOException: %s", e1.getMessage()));
                 }
 
             }
