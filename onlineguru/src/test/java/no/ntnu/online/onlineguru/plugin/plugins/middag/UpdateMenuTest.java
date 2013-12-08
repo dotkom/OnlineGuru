ecbe00bfa64beae0550ee577e82308c62d570e2c
melwil <slettvold@gmail.com>
2013-12-08 04:40:58 +0100

    Added tests for middag.

---
+++ onlineguru/src/test/java/no/ntnu/online/onlineguru/plugin/plugins/middag/UpdateMenuTest.java
@@ -0,0 +1,18 @@
+package no.ntnu.online.onlineguru.plugin.plugins.middag;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Håvard Slettvold
+ */
+public class UpdateMenuTest {
+
+
+    @Test
+    public void testFindMenuItems() {
+
+        UpdateMenu.findMenuItems("");
+    }
+
+}
