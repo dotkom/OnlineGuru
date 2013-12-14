3b427751cf57c84a4aac5fff927811128aae894e
melwil <slettvold@gmail.com>
2013-12-14 19:41:41 +0100

    More work on the plugin logic. Finished command handling.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/mailannouncer/MailAnnouncerPlugin.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/mailannouncer/MailAnnouncerPlugin.java
@@ -1,13 +1,24 @@
 package no.ntnu.online.onlineguru.plugin.plugins.mailannouncer;
 
 import no.fictive.irclib.event.container.Event;
+import no.fictive.irclib.event.container.command.PrivMsgEvent;
+import no.fictive.irclib.event.model.EventType;
 import no.ntnu.online.onlineguru.OnlineGuru;
 import no.ntnu.online.onlineguru.plugin.control.EventDistributor;
 import no.ntnu.online.onlineguru.plugin.model.Plugin;
 import no.ntnu.online.onlineguru.plugin.model.PluginWithDependencies;
+import no.ntnu.online.onlineguru.plugin.plugins.flags.FlagsPlugin;
+import no.ntnu.online.onlineguru.plugin.plugins.flags.model.Flag;
+import no.ntnu.online.onlineguru.plugin.plugins.help.HelpPlugin;
+import no.ntnu.online.onlineguru.plugin.plugins.mailannouncer.listeners.CallbackListener;
 import no.ntnu.online.onlineguru.plugin.plugins.mailannouncer.listeners.Listeners;
 import no.ntnu.online.onlineguru.service.services.webserver.Webserver;
+import no.ntnu.online.onlineguru.utils.JSONStorage;
 import no.ntnu.online.onlineguru.utils.Wand;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Håvard Slettvold
@@ -16,18 +27,38 @@ public class MailAnnouncerPlugin implements PluginWithDependencies {
 
     private Wand wand;
 
+    private FlagsPlugin flagsPlugin;
+
+    private final String database_folder = "database/";
+    private final String database_file = database_folder + "mail.json";
+
+    private final Flag editorFlag = Flag.A;
+    private final Flag peekingFlag = Flag.a;
+
     private MailCallback mailCallback;
     private Listeners listeners;
+
+    private Pattern commandPattern = Pattern.compile(
+            "!mail" +                      // Trigger
+            " (\\w+)" +                   // Mailinglist, group 1
+            "(?: (#\\S+))?" +              // Channel, group 2
+            "(?: (on|off))?",              // Setting, group 3
+            Pattern.CASE_INSENSITIVE
+    );
+
+    public MailAnnouncerPlugin() {
+        // This needs to be initiated here for testing purposes, deal with it.
+        listeners = new Listeners();
+    }
 
     @Override
     public String[] getDependencies() {
         // Doing this setup in a method that is solely called by the PluginManager once.
         // The reason is we do not want to execute these actions during tests.
-//        listeners = storageManager.loadListeners();
-//        if (listeners == null) {
-//            listeners = new Listeners();
-//        }
-//        githubCallback = new GithubCallback(wand, listeners);
+        listeners = (Listeners) JSONStorage.load(database_file, Listeners.class);
+        if (listeners == null) {
+            listeners = new Listeners();
+        }
         mailCallback = new MailCallback(wand, listeners);
 
         // Running the registering with the web server async, it may take time.
@@ -47,7 +78,11 @@ public class MailAnnouncerPlugin implements PluginWithDependencies {
 
     @Override
     public void loadDependency(Plugin plugin) {
+        if (plugin instanceof FlagsPlugin) this.flagsPlugin = (FlagsPlugin) plugin;
+        if (plugin instanceof HelpPlugin) {
+            HelpPlugin helpPlugin = (HelpPlugin) plugin;
 
+        }
     }
 
     @Override
@@ -57,12 +92,75 @@ public class MailAnnouncerPlugin implements PluginWithDependencies {
 
     @Override
     public void incomingEvent(Event e) {
+        if (e.getEventType() == EventType.PRIVMSG) {
+            PrivMsgEvent pme = (PrivMsgEvent) e;
+            String reply = handleCommand(pme);
+            if (reply != null) {
+                wand.sendMessageToTarget(pme.getNetwork(), pme.getTarget(), "[mail] " + reply);
+            }
+        }
+    }
 
+    public String handleCommand(PrivMsgEvent e) {
+        String message = e.getMessage().toLowerCase();
+        if (!message.startsWith("!mail")) return null;
+
+        Matcher matcher = commandPattern.matcher(message);
+        if (!matcher.find()) return null;
+
+        // Find a channel, or return an error
+        String channel = matcher.group(2);
+        if (channel == null) {
+            if (e.isChannelMessage()) {
+                channel = e.getChannel();
+            }
+            else {
+                return "You must specify a channel when using this command in private messages.";
+            }
+        }
+
+        // Check if the user has permissions to access.
+        Set<Flag> flags = flagsPlugin.getFlags(e.getNetwork(), channel, e.getSender());
+        if (!flags.contains(peekingFlag) && !flags.contains(Flag.A)) {
+            return "You do not have access to use this command. It requires +a for info and +A to edit.";
+        }
+
+        String mailinglist = matcher.group(1).toLowerCase();
+        String setting = matcher.group(3);
+
+        // Get the callback listener for the specified mailinglist.
+        CallbackListener cl = listeners.get(mailinglist);
+        // If it doesn't exist, create a new one.
+        if (cl == null) {
+            cl = new CallbackListener();
+            listeners.put(mailinglist, cl);
+        }
+
+        if (matcher.group(3) == null) {
+            if (cl.isSubscribed(e.getNetwork(), channel)) {
+                return channel + " is subscribed to '" + mailinglist + "'.";
+            }
+            return channel + " is not subscribed to '" + mailinglist + "'.";
+        }
+        else if (matcher.group(3).equals("on")) {
+            if (!cl.createSubscription(e.getNetwork(), channel)) {
+                return channel + " is already subscribed to '" + mailinglist + "'.";
+            }
+            return channel + " is now subscribed to '" + mailinglist + "'.";
+        }
+        else if (matcher.group(3).equals("off")) {
+            if (!cl.deleteSubscription(e.getNetwork(), channel)) {
+                return channel + " is not subscribed to '" + mailinglist + "'.";
+            }
+            return channel + " is no longer subscribed to '" + mailinglist + "'.";
+        }
+
+        return null;
     }
 
     @Override
     public void addEventDistributor(EventDistributor eventDistributor) {
-
+        eventDistributor.addListener(this, EventType.PRIVMSG);
     }
 
     @Override
--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/mailannouncer/MailCallback.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/mailannouncer/MailCallback.java
@@ -2,9 +2,12 @@ package no.ntnu.online.onlineguru.plugin.plugins.mailannouncer;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
 import no.ntnu.online.onlineguru.plugin.plugins.mailannouncer.listeners.Listeners;
+import no.ntnu.online.onlineguru.plugin.plugins.mailannouncer.model.Mail;
 import no.ntnu.online.onlineguru.service.services.webserver.NanoHTTPD;
 import no.ntnu.online.onlineguru.service.services.webserver.NanoHTTPD.Method;
+import no.ntnu.online.onlineguru.service.services.webserver.NanoHTTPD.Response;
 import no.ntnu.online.onlineguru.service.services.webserver.WebserverCallback;
 import no.ntnu.online.onlineguru.utils.Wand;
 import org.apache.log4j.Logger;
@@ -35,16 +38,37 @@ public class MailCallback implements WebserverCallback {
     }
 
     @Override
-    public NanoHTTPD.Response serve(String uri, Method method,  Map<String, String> headers, Map<String, String> parms) {
-        System.out.println("Received mail call");
+    public Response serve(String uri, Method method,  Map<String, String> headers, Map<String, String> parms) {
+        Mail mail = null;
+        if (method == Method.POST && parms.containsKey("payload")) {
+            try {
+                gson.fromJson(parms.get("payload"), Mail.class);
+            } catch (JsonParseException e) {
+                logger.error("Failed to parse JSON from "+uri, e.getCause());
+                logger.error(parms.get("payload"));
+            }
+        }
 
+        if (mail != null) {
+            String mailinglist = mail.getMailinglist();
+            if (listeners.containsKey(mailinglist)) {
+                listeners.get(mailinglist).incomingMail(this, mail);
+            }
+            else {
+                logger.debug(String.format("No subscriptions for mailinglist '%s'", mail.getMailinglist()));
+            }
+        }
 
-        return new NanoHTTPD.Response(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "OK");
+        return new Response(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "OK");
     }
 
     @Override
     public void httpdServerShutdown(String message) {
         logger.error(message);
+    }
+
+    public void announceToIRC(String network, String channel, String output) {
+        wand.sendMessageToTarget(wand.getNetworkByAlias(network), channel, output);
     }
 
 }
