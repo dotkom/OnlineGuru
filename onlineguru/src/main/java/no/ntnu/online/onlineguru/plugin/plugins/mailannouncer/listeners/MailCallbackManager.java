3659b542ac19fcffb0192606525cfc6d6dc52995
melwil <slettvold@gmail.com>
2013-12-17 22:33:46 +0100

    Replaced the BiMap with a regular hashmap and consequently the way reversing is done.

--- onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/mailannouncer/listeners/MailCallbackManager.java
+++ onlineguru/src/main/java/no/ntnu/online/onlineguru/plugin/plugins/mailannouncer/listeners/MailCallbackManager.java
@@ -9,22 +9,23 @@ import java.util.Map;
 public class MailCallbackManager {
 
     private Map<String, String> aliases;
-    private Map<String, MailCallbackListener> mailCallbackListenerMap;
+    private Map<String, MailCallbackListener> mailCallbackListeners;
 
     public MailCallbackManager() {
-        this.mailCallbackListenerMap = new HashMap<String, MailCallbackListener>();
+        this.aliases = new HashMap<String, String>();
+        this.mailCallbackListeners = new HashMap<String, MailCallbackListener>();
     }
 
     public void put(String mailinglist, MailCallbackListener mailCallbackListener) {
-        mailCallbackListenerMap.put(mailinglist, mailCallbackListener);
+        mailCallbackListeners.put(mailinglist, mailCallbackListener);
     }
 
     public boolean containsKey(String mailinglist) {
-        return mailCallbackListenerMap.containsKey(mailinglist);
+        return mailCallbackListeners.containsKey(mailinglist);
     }
 
     public MailCallbackListener get(String mailinglist) {
-        return mailCallbackListenerMap.get(mailinglist);
+        return mailCallbackListeners.get(mailinglist);
     }
 
     public void addAlias(String mailinglist, String alias) {
@@ -33,6 +34,15 @@ public class MailCallbackManager {
 
     public String getAlias(String mailinglist) {
         return aliases.get(mailinglist);
+    }
+
+    public String getOriginal(String alias) {
+        for (Map.Entry<String, String> e : aliases.entrySet()) {
+            if (alias.equals(e.getValue())) {
+                return e.getKey();
+            }
+        }
+        return null;
     }
 
 }
