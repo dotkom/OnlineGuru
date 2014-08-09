afa6ea60e663adcd441dcbd45a85dedb46f42316
melwil <slettvold@gmail.com>
2014-08-09 22:55:53 +0200

    Changed a static link in the runfile to point to the correct folder.

--- runbot.sh
+++ runbot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
-java -Dlog4j.debug -Dlog4j.configuration=file:///local/home/sys/onlinebot/log4j.properties -jar bot/onlineguru/target/onlineguru-1.2-SNAPSHOT-jar-with-dependencies.jar &
+java -Dlog4j.debug -Dlog4j.configuration=file:///local/home/sys/onlineguru/log4j.properties -jar bot/onlineguru/target/onlineguru-1.2-SNAPSHOT-jar-with-dependencies.jar &
 cd bot/
